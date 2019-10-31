625a6d11886554210085a70fa9987cdb71ef363a
carl <carl.r.montanari@gmail.com>
2019-10-31 14:05:15 -0700

    exercise1 drafts

---
+++ class1/exercises/exercise1/print_yaml.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from pprint import pprint
+import sys
+import yaml
+
+
+def read_yaml(filename):
+    with open(filename) as f:
+        return yaml.safe_load(f)
+
+
+if __name__ == "__main__":
+    try:
+        filename = sys.argv[1]
+    except IndexError as e:
+        raise e("You need to specify the file you wish to print!")
+    print(read_yaml(filename))
