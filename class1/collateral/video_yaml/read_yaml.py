9d940ba81615b540e24ab82de7650164427c938d
Kirk Byers <ktbyers@twb-tech.com>
2019-11-06 12:19:38 -0800

    Lesson1 collateral

---
+++ class1/collateral/video_yaml/read_yaml.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import yaml
+import json
+from pprint import pprint
+
+
+def read_yaml(filename):
+    with open(filename) as f:
+        return yaml.safe_load(f)
+
+
+def write_yaml(filename, output, style=None):
+    with open(filename, "wt") as f:
+        if style == "compressed":
+            yaml.dump(output, f, default_flow_style=True)
+        else:
+            yaml.dump(output, f, default_flow_style=False)
+
+
+if __name__ == "__main__":
+    file_name = input("Enter your YAML filename: ")
+    yaml_out = read_yaml(file_name)
+    pprint(yaml_out)
+
+    # print(json.dumps(yaml_out))
