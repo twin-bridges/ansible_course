bc19e5792fbe23a7b4602ef2a7147d1a63ff71ab
Kirk Byers <ktbyers@twb-tech.com>
2020-02-10 14:31:08 -0800

    Class5 tests

---
+++ tests/test_class5.py
@@ -0,0 +1,105 @@
+import os
+import subprocess
+import re
+
+
+def subprocess_runner(cmd_list, exercise_dir):
+    with subprocess.Popen(
+        cmd_list, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=exercise_dir
+    ) as proc:
+        std_out, std_err = proc.communicate()
+    return (std_out.decode(), std_err.decode(), proc.returncode)
+
+def remove_ansible_warnings(std_err):
+    """Remove the specified warnings from std_err."""
+    warning_list = [
+        r"^.WARNING.: Ignoring timeout.10. for .*$",
+    ]
+
+    # Remove warnings one at a time from std_err
+    for ansible_warn in warning_list:
+        std_err = re.sub(ansible_warn, "", std_err, flags=re.M)
+
+    return std_err.strip()
+
+
+def test_class5_ex1():
+
+    base_path = "../class5/exercises/exercise1"
+    cmd_list = ["ansible-playbook", "exercise1.yml", "--forks 12"]
+    std_out, std_err, return_code = subprocess_runner(cmd_list, exercise_dir=base_path)
+    std_err = remove_ansible_warnings(std_err)
+
+    assert "TASK [IOS global config]" in std_out
+    assert "TASK [IOS-XE global config]" in std_out
+    assert "TASK [EOS global config]" in std_out
+    assert "TASK [NX-OS global config]" in std_out
+    assert "TASK [Juniper global config]" in std_out
+    for device in ["cisco1", "cisco2", "cisco5", "cisco6", "arista5", "arista6",
+                   "arista7", "arista8", "nxos1", "nxos2"]:
+        assert std_out.count(device) == 6
+    for device in ["srx1"]:
+        assert std_out.count(device) == 2
+
+    assert std_err == ""
+    assert return_code == 0
+
+def test_class5_ex1_idempotent():
+    """Run test again to verify idempotency."""
+    base_path = "../class5/exercises/exercise1"
+    cmd_list = ["ansible-playbook", "exercise1.yml", "--forks 12"]
+    std_out, std_err, return_code = subprocess_runner(cmd_list, exercise_dir=base_path)
+    for device in ["cisco1", "cisco2", "cisco5", "cisco6", "arista5", "arista6",
+                   "arista7", "arista8", "nxos1", "nxos2", "srx1"]:
+        pattern = rf"^{device}.*changed=0 .*$"
+        assert re.search(pattern, std_out, flags=re.M)
+
+def test_class5_ex2():
+    base_path = "../class5/exercises/exercise2"
+    cmd_list = ["ansible-playbook", "exercise2.yml", "--forks 12"]
+    std_out, std_err, return_code = subprocess_runner(cmd_list, exercise_dir=base_path)
+    std_err = remove_ansible_warnings(std_err)
+    assert std_err == ""
+    assert return_code == 0
+
+    # Should be idempotent as would have been configured in exercise1.
+    for device in ["cisco1", "cisco2", "cisco5", "cisco6", "arista5", "arista6",
+                   "arista7", "arista8", "nxos1", "nxos2", "srx1"]:
+        pattern = rf"^{device}.*changed=0 .*$"
+        assert re.search(pattern, std_out, flags=re.M)
+
+def test_class5_ex3a():
+    base_path = "../class5/exercises/exercise3"
+    cmd_list = ["ansible-playbook", "exercise3a.yml"]
+    std_out, std_err, return_code = subprocess_runner(cmd_list, exercise_dir=base_path)
+    std_err = remove_ansible_warnings(std_err)
+    assert std_out.count("permit") == 20
+    assert std_err == ""
+    assert return_code == 0
+
+def test_class5_ex3b():
+    base_path = "../class5/exercises/exercise3"
+    cmd_list = ["ansible-playbook", "exercise3b.yml"]
+    std_out, std_err, return_code = subprocess_runner(cmd_list, exercise_dir=base_path)
+    std_err = remove_ansible_warnings(std_err)
+    assert std_out.count("permit") == 18
+    assert std_out.count("deny") == 2
+    assert std_err == ""
+    assert return_code == 0
+
+#def test_class4_ex4():
+#    base_path = "../class4/exercises/exercise4"
+#    cmd_list = ["ansible-playbook", "exercise4.yml"]
+#    std_out, std_err, return_code = subprocess_runner(cmd_list, exercise_dir=base_path)
+#    assert std_err == ""
+#    assert return_code == 0
+#
+#def test_class4_ex5():
+#    # Deprecated module will generate an ansible warning on std_err (disable that)
+#    os.environ["ANSIBLE_DEPRECATION_WARNINGS"] = "False"
+#    base_path = "../class4/exercises/exercise5"
+#    cmd_list = ["ansible-playbook", "exercise5.yml"]
+#    std_out, std_err, return_code = subprocess_runner(cmd_list, exercise_dir=base_path)
+#    assert std_err == ""
+#    assert return_code == 0
+#    os.environ.pop("ANSIBLE_DEPRECATION_WARNINGS")
